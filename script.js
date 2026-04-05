739ee9c93af6f355fee1dd16b7a1ffdafd079c62
rafflores17-prog <rafflores17@gmail.com>
2026-04-05 09:54:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,60 @@
+// NAVEGAÇÃO
+function abrir(id){
+  document.querySelectorAll('.tela').forEach(t => t.classList.remove('ativa'));
+  document.getElementById(id).classList.add('ativa');
+}
 
+function voltar(){
+  abrir('home');
+}
+
+// FRASES
+let frases = [
+  "Você é mais forte do que imagina.",
+  "Tudo pode mudar em um dia.",
+  "Confie no seu processo.",
+  "Grandes coisas levam tempo.",
+  "Continue, mesmo sem motivação.",
+  "Você já venceu muita coisa até aqui."
+];
+
+let ultimaFrase = "";
+
+function gerarFrase(){
+  let nova;
+
+  do {
+    nova = frases[Math.floor(Math.random() * frases.length)];
+  } while(nova === ultimaFrase);
+
+  ultimaFrase = nova;
+
+  document.getElementById("textoFrase").innerText = nova;
+}
+
+function copiarFrase(){
+  let texto = document.getElementById("textoFrase").innerText;
+  navigator.clipboard.writeText(texto);
+  alert("Frase copiada!");
+}
+
+// SORTE DO DIA
+let sortes = [
+  "Hoje será um dia incrível.",
+  "Uma surpresa está chegando.",
+  "Algo bom vai acontecer.",
+  "Evite decisões impulsivas hoje.",
+  "Alguém pensa em você."
+];
+
+function gerarSorte(){
+  let hoje = new Date().toDateString();
+  let salva = localStorage.getItem("sorte_"+hoje);
+
+  if(!salva){
+    salva = sortes[Math.floor(Math.random() * sortes.length)];
+    localStorage.setItem("sorte_"+hoje, salva);
+  }
+
+  document.getElementById("textoSorte").innerText = salva;
+}
